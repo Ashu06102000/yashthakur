429f022bc7da1cd0030f2b6370a1481186c4ddae
yash-copods <yash.thakur@copods.co>
2024-12-29 13:45:43 +0530

    started about us section

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -1,7 +1,23 @@
 const About = () => {
   return (
-    <div>
-      <div>about</div>
+    <div className="flex ">
+      <div className="h-full w-1/4"></div>
+      <div className="border-[1px] border-t-0 border-ra w-auto h-full p-8">
+        <div className="h-[100vh] relative flex justify-center">
+          <video
+            className="h-56 absolute top-[14%]"
+            src="/src/assets/robo.mp4"
+            autoPlay
+            muted
+          ></video>
+          <img
+            className="sticky top-0 max-h-[650px] w-full max-w-[400px]"
+            src="/src/assets/elivat.webp"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="h-full w-1/4"></div>
     </div>
   );
 };
