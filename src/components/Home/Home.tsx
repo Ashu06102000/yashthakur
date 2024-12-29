5c3ad8ccc502846b9fc1a52ddd21c5567ca791f6
yash-copods <yash.thakur@copods.co>
2024-12-29 13:33:02 +0530

    added assets

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,11 @@
 import Hero from "./Hero";
+import About from "./About";
 
 const Home = () => {
   return (
     <div className="w-full h-full flex flex-col">
       <Hero />
+      <About />
     </div>
   );
 };
