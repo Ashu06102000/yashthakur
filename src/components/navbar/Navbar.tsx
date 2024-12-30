fc33fb0da36ed8cb15b436a9a346a7552d6695bb
yash-copods <yash.thakur@copods.co>
2024-12-30 13:04:05 +0530

    fixed

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,7 @@
 const Navbar = () => {
-  const NavItems = ["About", "Contact"];
   return (
-    <div className="w-full border-bot py-6 px-6 font-semibold flex justify-between">
-      <h2 className="uppercase text-4xl">Y-ashu</h2>
-      <div className="flex gap-8">
-        {NavItems.map((item) => (
-          <div className="font-normal text-xl">{item}</div>
-        ))}
-      </div>
+    <div className="w-full border-bot py-6 px-16 font-semibold flex justify-between">
+      <h2 className="uppercase text-2xl">YASH.06</h2>
     </div>
   );
 };
