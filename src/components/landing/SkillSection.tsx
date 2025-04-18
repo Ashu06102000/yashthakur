5d824b5094501c4dda07758b3f283beefce90f33
yash-copods <yash.thakur@copods.co>
2025-04-18 15:19:00 +0530

    skillsection responsiveness

--- src/components/landing/SkillSection.tsx
+++ src/components/landing/SkillSection.tsx
@@ -21,7 +21,8 @@ const groupedSkills = [
 
 const SkillSection = () => {
   const listRef = useRef(null);
-  const itemRefs = useRef([]);
+  const itemRefs = useRef<any[]>([]);
+
   useEffect(() => {
     const ctx = gsap.context(() => {
       gsap.fromTo(
@@ -50,32 +51,27 @@ const SkillSection = () => {
   }, []);
 
   return (
-    <section className=" px-10 sm:px-20 py-24 flex items-start justify-between">
+    <section className="px-6 sm:px-10 md:px-20 py-24 flex flex-col md:flex-row gap-12 md:gap-24 items-start justify-between">
       {/* Sticky Heading Block */}
-      <div className="text-left flex justify-between sticky top-96">
-        <div className="flex flex-col gap-0 font-oreni">
-          <h2 className="text-2xl font-semibold uppercase text-black">
+      <div className="text-left md:sticky md:top-32 flex-shrink-0 w-full md:w-1/3">
+        <div className="flex flex-col gap-2 font-oreni">
+          <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold uppercase text-black">
             Capabilities
           </h2>
-          <p className="text-xl font-light text-black">
+          <p className="text-lg sm:text-xl md:text-2xl font-light text-black">
             Here's what I bring to the table
           </p>
         </div>
       </div>
 
-      <div className="relative w-2/3" ref={listRef}>
-        <ul className="space-y-4 text-2xl font-light gap-1 flex flex-col">
+      {/* Skills List */}
+      <div className="relative w-full md:w-2/3" ref={listRef}>
+        <ul className="space-y-4 flex flex-col">
           {groupedSkills.map((skill, i) => (
             <li
               key={skill}
-              ref={(el) => {
-                if (itemRefs.current) {
-                  //@ts-ignore
-                  itemRefs.current[i] = el;
-                }
-              }}
-              className="relative list-none text-black transition-all duration-500 
-                         hover:font-semibold hover:-translate-x-24 text-3xl leading-none"
+              ref={(el) => (itemRefs.current[i] = el)}
+              className="relative list-none text-black text-2xl sm:text-3xl font-light transition-all duration-500 hover:font-semibold hover:-translate-x-6 sm:hover:-translate-x-12 md:hover:-translate-x-24 leading-snug"
             >
               {skill}
             </li>
