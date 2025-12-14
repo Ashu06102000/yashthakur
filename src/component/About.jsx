42916f7d367b45b9e1c731e38f1ff1ab06962b8a
yash-copods <yash.thakur@copods.co>
2025-12-15 01:18:07 +0530

    fixed

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,47 +1,39 @@
-import React, { useEffect, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const About = () => {
-  const contentRef = useRef(null);
-
-  useEffect(() => {
-    if (contentRef.current) {
-      const paragraphs = contentRef.current.children;
-      gsap.fromTo(
-        paragraphs,
-        { opacity: 0, y: 30 }, // start state
-        {
-          opacity: 1,
-          y: 0, // end state
-          stagger: 0.2,
-          duration: 0.8,
-          ease: "power3.out",
-        }
-      );
-    }
-  }, []);
-
   return (
-    <div className="w-full h-[60vh] flex justify-center items-center px-4">
-      <div className="w-full max-w-4xl gap-6 h-full">
-        <div
-          ref={contentRef}
-          className="w-full flex flex-col justify-center gap-3"
-        >
-          <p className="text-white text-3xl  font-normal lineheight">
-            I specialize in Product Experience Engineering, crafting solutions
-            that prioritize exceptional user experiences. I collaborate closely
-            with cross-functional teams to turn business goals into impactful
-            products.
-          </p>
-          <p className="text-white text-3xl font-normal lineheight">
-            Skilled in full-stack development and AWS infrastructure, I
-            translate complex requirements into scalable solutions. Passionate
-            about problem-solving and leveraging technology to drive innovation.
-          </p>
-        </div>
+    <section className="flex flex-col md:flex-row gap-5 md:gap:0 justify-between">
+      <h3 className="text-white text-lg">About Me</h3>
+
+      <div className="relative md:w-2/3 w-full flex flex-col gap-4 text-left">
+        <p className="tracking-tight font-light text-base text-white m-0 text-opacity-55">
+          With expertise in Product Experience Engineering, I thrive on
+          exploring cutting-edge technologies and crafting solutions that
+          prioritize exceptional user experiences..
+        </p>
+        <p className="tracking-tight font-light text-base text-white m-0 text-opacity-55">
+          I collaborate closely with cross-functional teams, aligning business
+          objectives with user needs to build innovative and impactful products.
+        </p>
+        <p className="tracking-tight font-light text-base text-white m-0 text-opacity-55">
+          Proficient in full-stack development, including infrastructure
+          management with AWS, I seamlessly translate complex requirements into
+          scalable solutions.
+        </p>
+        <p className="tracking-tight font-light text-base text-white m-0 text-opacity-55">
+          Experienced in fast-paced startup ecosystems, I excel in adapting to
+          challenges, driving technical excellence, and enhancing product
+          strategy.
+        </p>
+        <p className="tracking-tight font-light text-base text-white m-0 text-opacity-55">
+          Passionate about creative problem-solving and leveraging technology to
+          push boundaries in product development.
+        </p>
       </div>
-    </div>
+    </section>
   );
 };
 
